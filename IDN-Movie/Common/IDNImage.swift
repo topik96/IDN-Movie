343cb2a37dffda428827b2dc25c51d856f599dda
Topik Mujianto <topik.mujianto@finaccel.co>
2023-12-03 17:50:34 +0700

    BugFix Failed Run Xcode 14

--- IDN-Movie/Common/IDNImage.swift
+++ IDN-Movie/Common/IDNImage.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-enum IDNIcon {
+enum IDNIcon: String {
     case logo
     case home
     case search
@@ -24,29 +24,6 @@ enum IDNIcon {
 
 struct IDNImage {
     func getImage(_ icon: IDNIcon) -> UIImage? {
-        switch icon {
-        case .logo:
-            return UIImage(named: "logo")
-        case .home:
-            return UIImage(named: "home")
-        case .search:
-            return UIImage(named: "search")
-        case .save:
-            return UIImage(named: "save")
-        case .filter:
-            return UIImage(named: "filter")
-        case .saveRed:
-            return UIImage(named: "saveRed")
-        case .saveWhite:
-            return UIImage(named: "saveWhite")
-        case .barSignal:
-            return UIImage(named: "barSignal")
-        case .sad:
-            return UIImage(named: "sad")
-        case .noData:
-            return UIImage(named: "noData")
-        case .trashRedFill:
-            return UIImage(named: "trashRedFill")
-        }
+        return UIImage(named: icon.rawValue)
     }
 }
